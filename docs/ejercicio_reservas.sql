71b83eb0a8de6ee46761540f7b405742be3b0165
juna101 <juangu.edu@gmail.com>
2023-11-02 11:39:42 +0100

    actualizado proyecto

---
+++ docs/ejercicio_reservas.sql
@@ -0,0 +1,8 @@
+
+usuario(username, password, email, nombre, apellido, teléfono)
+
+reserva(usuario, horario, fecha)
+
+horario(hora_inicio, hora_fin, instalación)
+
+instalación(nombre, descripción)
